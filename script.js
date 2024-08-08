bf54d7bc56f00cdbde3ed7e04c0ba66a2be12012
Thomas Wilson <t.wilson@dundeeandangus.ac.uk>
2024-08-08 10:53:12 +0100

    Updated

---
+++ script.js
@@ -0,0 +1,17 @@
+/**
+ * JavaScript File.
+ * Your code can go here
+ */
+
+let num1 = 46;
+let num2 = 32;
+
+let sum = num1 + num2; // addition
+let difference = num1 - num2; // subtraction
+let product = num1 * num2; // multiplication
+let quotient = num1 / num2; // division
+let modulus = num1 % num2; // remainder.
+let exponent = num1 ** num2; // to the power of.
+
+num1++; // increment by 1.
+num1--; // decrement by 1.
